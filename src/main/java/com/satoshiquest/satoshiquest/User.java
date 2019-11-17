54abf06b2f7a886b8a4a4c0918f5f4d9f92fc6a6
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2019-11-17 18:47:34 -0500

    address fixes

--- src/main/java/com/satoshiquest/satoshiquest/User.java
+++ src/main/java/com/satoshiquest/satoshiquest/User.java
@@ -14,21 +14,38 @@ public class User {
 
   public User(Player player)
       throws ParseException, org.json.simple.parser.ParseException, IOException {
-    this.player = player;
-	if (satoshiQuest.REDIS.exists("nodeWallet"+this.player.getUniqueId().toString())) {
-		try {
-			this.wallet = satoshiQuest.loadWallet(satoshiQuest.REDIS.get("nodeWallet"+this.player.getUniqueId().toString()));
-		        System.out.println("[user wallet] trying to load node wallet");
-		} catch (NullPointerException npe) {
-			npe.printStackTrace();
-			System.out.println("[user wallet] wallet not found, attempting to create.");
+	this.player = player;
+	this.wallet = null;
+	satoshiQuest.getWalletInfo(player.getUniqueId().toString());
+	if (!satoshiQuest.REDIS.exists("nodeWallet"+player.getUniqueId().toString())) {
+		if (satoshiQuest.loadWallet(player.getUniqueId().toString())!=null) {
+			try {
+				wallet = satoshiQuest.loadWallet(satoshiQuest.REDIS.get("nodeWallet"+player.getUniqueId().toString()));
+			        System.out.println("[world wallet] trying to load node wallet");
+			} catch (NullPointerException npe) {
+				npe.printStackTrace();
+				System.out.println("[world wallet] wallet not found, attempting to create.");
+			}
+		} else {
+	        	wallet = satoshiQuest.generateNewWallet(player.getUniqueId().toString());
+        		System.out.println("[world wallet] generated new wallet");
+			satoshiQuest.REDIS.set("nodeWallet"+player.getUniqueId().toString(),player.getUniqueId().toString());
+		} 
+	} else { 
+		wallet = satoshiQuest.loadWallet(satoshiQuest.REDIS.get("nodeWallet"+player.getUniqueId().toString()));
+	}//nodewallet
+	if (!satoshiQuest.REDIS.exists("nodeAddress"+player.getUniqueId().toString())) {
+	try {
+		if (wallet.getAccountAddress()!=null) {
+			satoshiQuest.REDIS.set("nodeAddress"+player.getUniqueId().toString(),wallet.getAccountAddress());
+		} else {
+			satoshiQuest.REDIS.set("nodeAddress"+player.getUniqueId().toString(),wallet.getNewAccountAddress());
 		}
-	} else if(!satoshiQuest.REDIS.exists("nodeWallet"+this.player.getUniqueId().toString()))
-	{
-	        this.wallet = satoshiQuest.generateNewWallet(this.player.getUniqueId().toString());
-        	System.out.println("[user wallet] generated new wallet");
-		satoshiQuest.REDIS.set("nodeWallet"+this.player.getUniqueId().toString(),this.player.getUniqueId().toString());
-	}
-  }
+	} catch (NullPointerException npe2) {
+			npe2.printStackTrace();
+			System.out.println("[world address] address not found, attempting to create.");
+		}
+	}//endAddress
 
+  }
 }
