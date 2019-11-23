81ca348d48950feeb4db3bbe76385ddca7ad4862
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2019-11-23 14:53:22 -0500

    consistancies

--- src/main/java/com/satoshiquest/satoshiquest/NodeWallet.java
+++ src/main/java/com/satoshiquest/satoshiquest/NodeWallet.java
@@ -170,8 +170,82 @@ public class NodeWallet {
     return response_object.get("result").toString();
   }
 
-  public Long getBalance(int confirmations) {
+  public Long getBalance(int confirmations) throws IOException, org.json.simple.parser.ParseException {
+	try {
+        JSONParser parser = new JSONParser();
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("jsonrpc", "1.0");
+        jsonObject.put("id", "satoshiquest");
+        jsonObject.put("method", "getbalance");
+        JSONArray params = new JSONArray();
+	params.add("*");
+	params.add(confirmations);
+        System.out.println("Parms: " + params);
+        jsonObject.put("params", params);
+        URL url = new URL("http://" + SatoshiQuest.BITCOIN_NODE_HOST + ":" + SatoshiQuest.BITCOIN_NODE_PORT + "/wallet/" + account_id);
+        System.out.println(url.toString());
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        String userPassword = SatoshiQuest.BITCOIN_NODE_USERNAME + ":" + SatoshiQuest.BITCOIN_NODE_PASSWORD;
+        String encoding = Base64.getEncoder().encodeToString(userPassword.getBytes());
+        con.setRequestProperty("Authorization", "Basic " + encoding);
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "Mozilla/1.22 (compatible; MSIE 2.0; Windows 3.1)");
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        con.setDoOutput(true);
+        OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
+        System.out.println(jsonObject.toString());
+        out.write(jsonObject.toString());
+        out.close();
+
+        if(con.getResponseCode()==200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+            JSONObject response_object = (JSONObject) parser.parse(response.toString());
+	    Double d = Double.parseDouble(response_object.get("result").toString().trim()) * 100000000L;
+	    final Long balance = d.longValue();
+            System.out.println(balance);
+	    return balance;
+
+        } else {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+            JSONObject response_object = (JSONObject) parser.parse(response.toString());
+	    Double d = Double.parseDouble(response_object.get("result").toString().trim()) * 100000000L;
+	    final Long balance = d.longValue();
+            System.out.println(balance);
+	    return balance;
+        }
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+	return 0L;
+   }
+
+
+}
+
+
+/*
+{
     System.out.println("getbalance: " + account_id);
+    System.out.println("balanceaddress: " + address);
     try {
       JSONParser parser = new JSONParser();
       final JSONObject jsonObject = new JSONObject();
@@ -181,8 +255,9 @@ public class NodeWallet {
       JSONArray params = new JSONArray();
       params.add("*");
       params.add(confirmations);
+      //params.add(confirmations);
       jsonObject.put("params", params);
-      URL url = new URL("http://" + SatoshiQuest.BITCOIN_NODE_HOST + ":" + SatoshiQuest.BITCOIN_NODE_PORT + "/wallet/" + this.account_id);
+      URL url = new URL("http://" + SatoshiQuest.BITCOIN_NODE_HOST + ":" + SatoshiQuest.BITCOIN_NODE_PORT + "/wallet/" + account_id);
       HttpURLConnection con = (HttpURLConnection) url.openConnection();
       con.setConnectTimeout(5000);
       String userPassword = SatoshiQuest.BITCOIN_NODE_USERNAME + ":" + SatoshiQuest.BITCOIN_NODE_PASSWORD;
@@ -218,5 +293,4 @@ public class NodeWallet {
       return Long.valueOf(0);
     }
   }
-}
-
+*/
