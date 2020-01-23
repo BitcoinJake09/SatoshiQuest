9d50e1e8d6f8c1c153ff687f2edacddb1dceeb66
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2020-01-22 22:49:04 -0500

    does a full reset now
    will reset the loot maps, loot placement, spawn, player datas.

--- src/main/java/com/satoshiquest/satoshiquest/commands/ResetCommand.java
+++ src/main/java/com/satoshiquest/satoshiquest/commands/ResetCommand.java
@@ -12,6 +12,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.advancement.*;
 import java.util.*;
 
 
@@ -24,12 +31,38 @@ public class ResetCommand extends CommandAction {
     public boolean run(CommandSender sender, Command cmd, String label, String[] args, Player player) {
 	if (args.length == 0) {
 		satoshiQuest.REDIS.del("lootSpawnY");
+		satoshiQuest.REDIS.del("spawnCreated");
 		satoshiQuest.REDIS.set("gameRound","1");
+		satoshiQuest.REDIS.set("winner","true");
+	    for(OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
+		satoshiQuest.REDIS.set("ClearInv" +offlinePlayer.getUniqueId().toString(), "true");
+		}
+
+	for(Player p : Bukkit.getServer().getWorld(satoshiQuest.SERVERDISPLAY_NAME).getPlayers()) {
+                p.teleport(Bukkit.getWorlds().get(0).getSpawnLocation());
+		satoshiQuest.REDIS.del("ClearInv" +p.getUniqueId().toString());
+		PlayerInventory pli1= p.getInventory();
+		pli1.clear(); //delete player world datas
+		pli1.setArmorContents(new ItemStack[4]);
+		Inventory pe1  = p.getEnderChest();
+		pe1.clear();
+		p.setLevel(0);
+		p.setExp(0);
+		p.setExhaustion(0);
+		p.setFoodLevel(20);
+		p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+	}
 	    for(OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
 		if (satoshiQuest.REDIS.exists("LootAnnounced" +offlinePlayer.getUniqueId().toString())) {
 		satoshiQuest.REDIS.del("LootAnnounced" +offlinePlayer.getUniqueId().toString());
 		}
 		}
+            	Bukkit.getServer().unloadWorld(Bukkit.getServer().getWorld(satoshiQuest.SERVERDISPLAY_NAME), false);
+Bukkit.getServer().unloadWorld(Bukkit.getServer().getWorld(satoshiQuest.SERVERDISPLAY_NAME+"_the_end"), false);
+Bukkit.getServer().unloadWorld(Bukkit.getServer().getWorld(satoshiQuest.SERVERDISPLAY_NAME+"_nether"), false);
+		satoshiQuest.deleteLootWorlds();
+		satoshiQuest.onEnable();
+		satoshiQuest.REDIS.del("winner");
 		return true;	
 	} else {
         	return false;
