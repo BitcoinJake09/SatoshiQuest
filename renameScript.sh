348354b1a1357cb049442aef329df5bb8174ed74
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2020-02-23 17:44:05 -0500

    small fix

--- renameScript.sh
+++ renameScript.sh
@@ -1,8 +1,8 @@
-find . -type f -name "*.*" -exec sed -i'' -e 's/satoshiquest/changeme/g' {} +
-find . -type f -name "*.*" -exec sed -i'' -e 's/SatoshiQuest/ChangeMe/g' {} +
-find . -type f -name "*.*" -exec sed -i'' -e 's/Satoshiquest/Changeme/g' {} +
-find . -type f -name "*.*" -exec sed -i'' -e 's/satoshiQuest/changeMe/g' {} +
-find . -type f -name "*.*" -exec sed -i'' -e 's/SATOSHIQUEST/CHANGEME/g' {} +
+find . -type f -name "*" -exec sed -i'' -e 's/satoshiquest/changeme/g' {} +
+find . -type f -name "*" -exec sed -i'' -e 's/SatoshiQuest/ChangeMe/g' {} +
+find . -type f -name "*" -exec sed -i'' -e 's/Satoshiquest/Changeme/g' {} +
+find . -type f -name "*" -exec sed -i'' -e 's/satoshiQuest/changeMe/g' {} +
+find . -type f -name "*" -exec sed -i'' -e 's/SATOSHIQUEST/CHANGEME/g' {} +
 shopt -s globstar
 find . * | rename 's/satoshiquest/changeme/g'
 shopt -s globstar
